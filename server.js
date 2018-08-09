0376c2b121cdda796d4d78fd2ccc011027437cc3
Kyle Carney <kyle.e.carney@gmail.com>
2018-08-08 21:16:35 -0400

    friend finder update

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-// Dependencies
+//set up dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
+//creates express server and sets up a port
+var app = express(); 
+var port = process.env.PORT || 3000; 
 
-// Create an instance of the express app.
+//Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
+// Static files
+// needs to be called before the routes in order to work
+app.use(express.static('app/public'));
 
-// Set the port of our application
+//Router
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
 
+//Listening to the port that was set up
+app.listen(port, () => console.log("Listening on port %s", port));
